3b946436d5258129c724d6731323184f7ecd8939
nabm-utn <nabm.utn@gmail.com>
2021-08-03 16:11:11 -0300

    add Book creation and display logic

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
-function Book(title, author, pages, read=false){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read=read;
+let Library = [];
+const bookshelf = document.querySelector(".bookshelf");
 
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read": "not read yet"}`
+class Book {
+    constructor(title, author, pages, color, read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.color = color;
+        this.read = read;
+
+        this.info = function () {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`;
+        };
     }
 }
 
-let hobbit = new Book("The Hobbit", "Tolkien", 296)
-console.log(hobbit.info())
+function createNewBook() {
+    const title = document.getElementById("new-title").value;
+    const author = document.getElementById("new-author").value;
+    const pages = document.getElementById("new-pages").value;
+    const color = document.getElementById("new-color").value;
+    const book = new Book(title, author, pages, color);
+    Library.push(book);
+    updateBookshelf();
+}
 
-let Library = []
+function updateBookshelf() {
+    cleanBookshelf();
+    console.table(Library);
+    Library.forEach(book => {bookshelf.appendChild(createBookDiv(book))});
+}
 
-
-
-const bookshelf = document.querySelector(".bookshelf")
-
-for (let i = 0; i < 5; i++) {
-    let hobbitDisplay = document.createElement("div")
-    hobbitDisplay.classList.add("bookspine")
-    hobbitDisplay.textContent = hobbit.title
-    hobbitDisplay.style.backgroundColor = "olive"
-    bookshelf.appendChild(hobbitDisplay)
+function cleanBookshelf() {
+    while (bookshelf.firstChild) {
+        bookshelf.removeChild(bookshelf.lastChild);
     }
+}
 
+function createBookDiv(book) {
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("bookspine");
+    bookDiv.textContent = book.title;
+    bookDiv.style.backgroundColor = book.color;
+    bookDiv.addEventListener("click", e => {
+        console.log(e.target.info());
+    })
+    return bookDiv;
+}
+
+document.querySelector(".create-button").addEventListener("click", createNewBook);
+
+function randomHEX() {
+    const red = Math.floor(Math.random() * 256).toString(16);
+    const green = Math.floor(Math.random() * 256).toString(16);
+    const blue = Math.floor(Math.random() * 256).toString(16);
+    return `#${red}${green}${blue}`;
+}
+
+function randomizeColor() {
+    const color = document.getElementById("new-color");
+    color.value = randomHEX();
+}
+
+document.querySelector(".random-button").addEventListener("click", randomizeColor)
